c9668b292193b72daf7bba760591f9b7c4338a4e
unknown <hruales1138@gmail.com>
2022-11-07 20:21:10 -0500

    PI

--- api/src/controllers/pokemonController.js
+++ api/src/controllers/pokemonController.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const { where } = require('sequelize');
 const { Pokemon } = require('../db');
 
 async function getApiData() {
@@ -30,4 +31,85 @@ async function getApiData() {
     }
 };
 
-module.exports = { getApiData };
+async function getDbData() {
+    try {
+        let dbData = await Pokemon.findAll();
+        return dbData;
+        
+    } catch (error) {
+        return error.message
+    }
+};
+
+async function getAllData() {
+    try {
+        let apiData = await getApiData();
+        let dbData = await getDbData();
+    
+        let allData = apiData.concat(dbData);
+        return allData;
+        
+    } catch (error) {
+        return error.message
+    }
+};
+
+async function getDataByName(name) {
+    try {
+        let existing = (await axios('https://pokeapi.co/api/v2/pokemon?limit=1154')).data.results.map(e => e.name)
+        if(existing.includes(name)) {
+            let e = (await axios(`https://pokeapi.co/api/v2/pokemon/${name}`)).data
+            let apiData = {
+                id: e.id,
+                name: e.name,
+                hp: e.stats[0].base_stat,
+                attack: e.stats[1].base_stat,
+                defense: e.stats[2].base_stat,
+                speed: e.stats[3].base_stat,
+                height: e.height, 
+                weight: e.weight,
+                types: e.types.map(e => e.type.name),
+                image: e.sprites.other.dream_world.front_default
+            }
+            return apiData;
+        }
+        else {
+            let dbData = await Pokemon.findOne({ where: { name: name } })
+            return dbData
+        }
+        
+    } catch (error) {
+        return error.message
+    }
+};
+
+async function getDataById(id) {
+    try {
+        if(id.length < 10) {
+            let e = (await axios(`https://pokeapi.co/api/v2/pokemon/${parseInt(id)}`)).data
+            let apiData = {
+                id: e.id,
+                name: e.name,
+                hp: e.stats[0].base_stat,
+                attack: e.stats[1].base_stat,
+                defense: e.stats[2].base_stat,
+                speed: e.stats[3].base_stat,
+                height: e.height, 
+                weight: e.weight,
+                types: e.types.map(e => e.type.name),
+                image: e.sprites.other.dream_world.front_default
+            }
+            return apiData;
+        }
+        else {
+            let dbData = await Pokemon.findByPk(id)
+            return dbData
+        }
+        
+    } catch (error) {
+        return error.message
+    }
+};
+getDataById(2)
+
+module.exports = { getAllData, getDataByName, getDataById };
--- api/src/routes/pokemonRouter.js
+++ api/src/routes/pokemonRouter.js
@@ -1,12 +1,45 @@
 const { Router } = require('express');
 const { Pokemon } = require('../db');
-const { getApiData } = require('../controllers/pokemonController');
+const { getAllData, getDataByName, getDataById } = require('../controllers/pokemonController');
 
 const router = Router();
 
 router.get('/', async (req, res) => {
-    const allData = await getApiData();
-    res.send(allData)
+    try {
+        const { name } = req.query;
+        if(name) {
+            const dataByName = await getDataByName(name);
+            res.send(dataByName);
+        } 
+        else {
+            const allData = await getAllData();
+            res.send(allData)
+        }
+        
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const dataById = await getDataById(id)
+        res.send(dataById) 
+        
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const newPokemon = await Pokemon.create(req.body);
+        res.send(newPokemon)
+        
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
 })
 
 module.exports = router;
