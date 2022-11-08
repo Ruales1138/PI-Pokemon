aaf10111d80437de7dcd7c8d0fe7c0b55e4582d0
unknown <hruales1138@gmail.com>
2022-11-08 16:41:00 -0500

    PI

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -6,7 +6,7 @@ function Card(props) {
             <h1>{props.name.charAt(0).toUpperCase() + props.name.slice(1)}</h1>
             <img src={props.image} width="300" height="200" alt='image'/>
             <h4>Types:</h4>
-            {props.types.map(e => <p>{e.charAt(0).toUpperCase() + e.slice(1)}</p>)}
+            {props.types.map(e => <h4>{e.charAt(0).toUpperCase() + e.slice(1)}</h4>)}
             <hr/>
         </div>
     )
