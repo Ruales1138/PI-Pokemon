a3f1ac552997609cdb4b2d7207f4b432b08d2d19
unknown <hruales1138@gmail.com>
2022-11-22 14:02:39 -0500

    PI

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
+import style from './Card.module.css';
 
 function Card(props) {
     return(
         <div>
-            <h1>{props.name.charAt(0).toUpperCase() + props.name.slice(1)}</h1>
-            <img src={props.image} width='200' height="200" alt=''/>
-            <h4>Types:</h4>
-            {props.types.map(e => <h4 key={e}>{e.charAt(0).toUpperCase() + e.slice(1)}</h4>)}
-            <h4>
-                <Link to={`/pokemon/${props.id}`}>More info</Link>
+            <h1 className={style.card}>{props.name.charAt(0).toUpperCase() + props.name.slice(1)}</h1>
+            <img className={style.card} src={props.image} width='200' height="200" alt=''/>
+            <h4 className={style.card}>Types:</h4>
+            {props.types.map(e => <h4 className={style.card} key={e}>{e.charAt(0).toUpperCase() + e.slice(1)}</h4>)}
+            <h4 className={style.card}>
+                <Link className={style.link} to={`/pokemon/${props.id}`}>More info</Link>
             </h4>
         </div>
     )
